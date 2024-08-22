a00ffaecdd330e08b242530b69c268d34e9671fc
saiprasad <saiprasad.hegde@codecraft.co.in>
2024-08-22 16:36:47 +0530

    migrated into tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
+  theme: {
+    extend: {
+      screens: {
+        laptop: "1500px",
+        tablet: "1100px",
+        phone: "550px",
+      },
+    },
+  },
+  plugins: [],
+};
